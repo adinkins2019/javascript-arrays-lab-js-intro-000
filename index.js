0251f87b8af23d764ef90a03a57f9e39a39351ba
Antonio Dinkins <afdinki@gmail.com>
2019-12-11 03:20:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  var newArr = kittens;
+  newArr.push(name)
+  return newArr
+}
+
+function destructivelyPrependKitten(name){
+  
+}
+
+function destructivelyRemoveLastKitten(){
+  
+}
+
+function destructivelyRemoveFirstKitten(){
+  
+}
+
+function appendKitten(name){
+  
+}
+
+function prependKitten(name){
+  
+}
+
+function removeLastKitten(){
+  
+}
+
+function removeFirstKitten(){
+  
+}
